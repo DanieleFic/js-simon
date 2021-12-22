4b0f70108d3240edb58a7b2d422aaef692b724a2
DanieleFic <reb_ficus22@hotmail.it>
2021-12-22 16:05:26 +0100

    aggiunto il timer che fa sparire il div

--- js/myscript.js
+++ js/myscript.js
@@ -16,19 +16,28 @@ let numeriRandomArray = []; //creiamo un array vuoto in cui pushiamo i nostri nu
 let divNumeri = document.getElementById("numeri") //prendiamo il nostro div in html in cui mettiamo i numeri
 
 function generaNumeri(){ //faccio una function in cui generiamo i numeri e se esce un numero doppio ne genera un altro
-    let numeroGenerato = Math.floor(Math.random() * 30 + 1);
+    let numeroGenerato = Math.floor(Math.random() * 100 + 1);
     if(!numeriRandomArray.includes(numeroGenerato)){
-        numeriRandomArray.push(numeroGenerato)
+        numeriRandomArray.push(numeroGenerato);
     }else{
-
+        i--;
         console.log("hai generato un doppione");
     }
     console.log(numeroGenerato);
-    divNumeri.innerHTML += numeriRandomArray + " ";
+    divNumeri.innerHTML = numeriRandomArray + " ";
 }
 
 console.log(numeriRandomArray) //console log dell array dei numeri
 
 for(i=0; i < 5 ; i++){ //ciclo for in cui cicliamo 5 numeri
     generaNumeri()
+    
 }
+
+setTimeout(function (){
+    document.getElementById('numeri').classList.add("displaynone");
+    for(i=0; i < 5 ; i++){
+       
+    }
+}, 3000); 
+
